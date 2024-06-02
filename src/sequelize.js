9ce025277556660acfefcdc74131763b350a075a
rianabd01 <rianwahyuab@gmail.com>
2024-06-02 07:37:28 +0700

    fix kesalahan penulisan host

--- src/sequelize.js
+++ src/sequelize.js
@@ -1,7 +1,7 @@
 const { Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize('sure_app', 'root', 'yAle3iAiD', {
-  host: 'https://ec2-3-1-220-87.ap-southeast-1.compute.amazonaws.com/',
+  host: 'ec2-3-1-220-87.ap-southeast-1.compute.amazonaws.com',
   dialect: 'mysql',
 });
 
