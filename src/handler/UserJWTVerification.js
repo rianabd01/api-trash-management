18b89344cceae1a1407ee11cb9da82315b340516
rianabd01 <rianwahyuab@gmail.com>
2024-06-07 07:11:23 +0700

    fix if token invalid post with anonymous user

--- src/handler/UserJWTVerification.js
+++ src/handler/UserJWTVerification.js
@@ -6,11 +6,17 @@ dotenv.config();
 // eslint-disable-next-line consistent-return
 function getUserIdFromToken(req) {
   const auth = req.headers.authorization;
+
   if (auth) {
     const token = auth.split(' ')[1];
-    const decoded = jwt.verify(token, process.env.JWT_KEY);
-    console.log('token_decoded', decoded);
-    return decoded.id;
+    try {
+      const decoded = jwt.verify(token, process.env.JWT_KEY);
+
+      return decoded.id;
+    } catch (error) {
+      console.log('token invalid, you will be anonymous');
+      // return 'token invalid';
+    }
   }
 }
 
